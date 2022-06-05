e808174c1c30b6e221a31721cdeaaab06b424476
Cassiano Junior <cassianojuniorww@gmail.com>
2022-06-05 11:46:02 -0300

    Removing console logs

--- src/adapters/nodemailer/NodemailerMailAdapter.ts
+++ src/adapters/nodemailer/NodemailerMailAdapter.ts
@@ -1,14 +1,12 @@
-import nodemailer from "nodemailer";
-import { MailAdapter, SendMailData } from "../MailAdapter";
-require("dotenv/config");
+import nodemailer from 'nodemailer';
+import { MailAdapter, SendMailData } from '../MailAdapter';
+require('dotenv/config');
 
 const NODEMAILER_USERNAME = process.env.NODEMAILER_USERNAME;
 const NODEMAILER_PASSWORD = process.env.NODEMAILER_PASSWORD;
 
-console.log(NODEMAILER_USERNAME, NODEMAILER_PASSWORD);
-
 const transport = nodemailer.createTransport({
-  host: "smtp.mailtrap.io",
+  host: 'smtp.mailtrap.io',
   port: 2525,
   auth: {
     user: NODEMAILER_USERNAME,
@@ -19,8 +17,8 @@ const transport = nodemailer.createTransport({
 export class NodemailerMailAdapter implements MailAdapter {
   async sendMail({ subject, body }: SendMailData) {
     await transport.sendMail({
-      from: "Equipe Feedget <feedget@enterprise.com>",
-      to: "Cassiano Junior <cassianojuniorww@gmail.com>",
+      from: 'Equipe Feedget <feedget@enterprise.com>',
+      to: 'Cassiano Junior <cassianojuniorww@gmail.com>',
       subject,
       html: body,
     });
